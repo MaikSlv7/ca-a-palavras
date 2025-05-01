0d07608c7c321821149ce2363b2d4e619a0c87bd
Maik Silva <silvamaik52@gmail.com>
2025-05-01 00:51:31 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open('caca-palavras-cache').then(function(cache) {
+    caches.open("caca-palavras-cache").then(function(cache) {
       return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
+        "./",
+        "./index.html",
+        "./manifest.json"
       ]);
     })
   );
 });
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
